a25eaf001b9394ee4bc80381a49bdd25a93e0f1e
maria1888 <48070915+maria1888@users.noreply.github.com>
2022-05-25 01:42:56 -0600

    agregar using

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,5 +1,10 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 namespace UI
 {
     internal static class Program
