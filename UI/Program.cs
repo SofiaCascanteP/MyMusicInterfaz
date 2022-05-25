c662d9213da9e459db6a4c2c83ce8d6b5a21c2b2
maria1888 <48070915+maria1888@users.noreply.github.com>
2022-05-25 01:53:07 -0600

    Conexion interfsz

--- UI/Program.cs
+++ UI/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+
 namespace UI
 {
     internal static class Program
@@ -19,6 +20,14 @@ namespace UI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4004);
+
+            listen.Connect(connect);
+            Console.WriteLine("Cliente conectado al servidor");
+
+
         }
     }
 }
