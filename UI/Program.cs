6deff1d6e723b99a7e5741872221ca6ade1b3973
maria1888 <48070915+maria1888@users.noreply.github.com>
2022-05-27 22:56:11 -0600

    Nuevo prog

--- UI/Program.cs
+++ UI/Program.cs
@@ -10,6 +10,7 @@ namespace UI
 {
     internal static class Program
     {
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -18,15 +19,17 @@ namespace UI
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
 
             Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4004);
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3333);
 
             listen.Connect(connect);
             Console.WriteLine("Cliente conectado al servidor");
 
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
 
         }
     }
